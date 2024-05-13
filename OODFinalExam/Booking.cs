6e1c2494dd89090156d6d467cf3df659469dcfaf
Kian Reynolds <S00237055@atu.ie>
2024-05-13 13:05:31 +0100

    created classes

---
+++ OODFinalExam/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODFinalExam
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOFParticipants { get; set; }
+
+        
+
+    }
+
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
