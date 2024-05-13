2b23789ace48a5b102fd313b2b762eb4ef51c39e
Kian Reynolds <S00237055@atu.ie>
2024-05-13 13:34:11 +0100

    Created xaml

--- DataManagement/Program.cs
+++ DataManagement/Program.cs
@@ -11,7 +11,16 @@ namespace DataManagement
     {
         static void Main(string[] args)
         {
-            CustomerData 
+            CustomerData db = new CustomerData ("OOD_KianReynolds");
+
+            using (db)
+            {
+                Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
+                Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
+                Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
+
+
+            }
         }
     }
 }
